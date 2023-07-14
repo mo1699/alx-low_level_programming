84f62287f14e5f96c5344d8c76df53b7cec8b35d
mohamed_C <medo16125578@gmail.com>
2023-07-13 20:53:45 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,17 @@
  * main - Entery point
  *
  * Description: print a qout using write function
- *        write(int fd.const void *buf.size_t count)
- *Return:1 (not success)
+ *      ssize_t write(int fd.const void *buf.size_t count);
+ *
+ *Return: 1 (error)
 */
+
+
 int main(void)
 {	
-	char quo[] = "and that piece of art is useful\" - Dora korpar. 2015-10-19\n";
-	write(1,quo,59);
-	return (1);
-{
+	char quo[] = "and that piece of art is usefu\" - Dora Korpar,
+	     2015 - 1 0 - 19\n";
+
+	writ(1, quo, 59);
+return (1);
+}
