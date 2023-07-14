262b2b7f22bd62b2f19ff04c4d3154d8065281d9
mohamed_C <medo16125578@gmail.com>
2023-07-13 18:55:33 -0700

    main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-/**
- * main-Entery point
- * Description: A C program that prints with put function
- * Return: Always 0>>Success
- */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
