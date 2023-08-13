44ea72743ddebb9f72c2ab50f6e4cbf475d63f79
mohamed_C <medo16125578@gmail.com>
2023-08-13 02:58:06 -0700

    h

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == 0)
-			printf("%d", a[i]);
-		else
+		if (a[i] != '\0')
 			printf(", %d", a[i]);
+		else
+			printf("%d", a[i]);
 	}
 		printf("\n");
 }
