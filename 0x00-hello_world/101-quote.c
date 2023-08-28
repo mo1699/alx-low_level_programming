756d035383ea51b0dfae992b886f26ecd1988809
mohamed99 <medo16125578@gmail.com>
2023-08-29 01:00:07 +0300

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,9 @@ int main(void)
 	char *array = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
 	for (i= 0; array[i] != '\0'; i++)
+	{
 	printf("%c", array[i]);
+	}
 	printf("\n");
 	return (1);
 }
