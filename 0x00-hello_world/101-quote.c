178514b38764c749f046f10be2b35e10573e4c92
mohamed99 <medo16125578@gmail.com>
2023-08-29 00:54:11 +0300

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
@@ -9,6 +7,10 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    char *array = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    int i;
+    for (i= 0; array[i] != '\0'; i++)
+        printf("%c", array[i]);
+    printf("\n");
+    return (1);
 }
