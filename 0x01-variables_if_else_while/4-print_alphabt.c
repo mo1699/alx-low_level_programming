fa7676c5ad46c2a0e8ce96a11671020e2ed5c2a0
mohamed99 <medo16125578@gmail.com>
2023-08-31 08:35:47 -0700

    C - Hello, World

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,12 @@
  */
 int main(void)
 {
-	char ch = 'a';
+	char ch;
 
-	while (ch <= 'z')
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'e' && ch != 'q')
-		{
+		if (ch != 'q' && ch != 'e')
 			putchar(ch);
-		}
-		ch++;
 	}
 	putchar('\n');
 	return (0);
