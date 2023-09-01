dfb11c3a9acef7ccb06300f3f7cdd457bd167bea
mo1699 <mo1699@gmail.com>
2023-09-01 19:43:55 +0200

    C - Hello, World

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,9 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
-
-	i = 0;
-
-	while (i < 10)
-	{
 		char ch;
 		int i;
+
 		for (i = 0; i <= 10; i++)
 		{
 			for (ch = 'a' ; ch <= 'z'; ch++)
