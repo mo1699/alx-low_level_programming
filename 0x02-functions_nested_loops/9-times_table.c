0a218eac6de3f901a1ae4553bb9d6c43c546916d
mo1699 <mo1699@gmail.com>
2023-09-06 22:25:07 +0200

    C - Hello, World

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,22 @@
  */
 void times_table(void)
 {
-  int i;
-  int n;
-  
-  for (i =0; i <= 9; i++){
-    for(n = 0; n <= 9; n++){
-      int k= n*i;
-      if (n == 0)
-        printf("%d",k);
-        
-     else if (n != 0 && k < 10){
-       printf(",  %d",k);
-    
-    }
-      else if (n != 0 && k >= 10){
-          printf(", %d",k);
-      
-      } 
-   
-      
-    }
-    
-      printf("\n");
+	int i;
+	int n;
 
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+		int k = n * i;
 
-    
-  }
+			if (n == 0)
+				printf("%d", k);
+			else if (n != 0 && k < 10)
+				printf(",  %d", k);
+			else if (n != 0 && k >= 10)
+				printf(", %d", k);
 }
-
+			printf("\n");
+}
+}
