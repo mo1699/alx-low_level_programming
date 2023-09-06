8061c614873e1cc360b9e5c7c928e774f162d496
mo1699 <mo1699@gmail.com>
2023-09-06 23:57:06 +0200

    C - Hello, World

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@ void jack_bauer(void)
 {
 	int j, i;
 
-for (i = 0; i > 24; ++i)
-	for (j = 0; j > 60; ++j)
+for (i = 0; i < 24; ++i)
+	for (j = 0; j < 60; ++j)
 	{
 	_putchar((i / 10) + '0');
-	_putchar((i % 10 )+'0');
+	_putchar((i % 10) + '0');
 	_putchar(':');
 	_putchar((j / 10) + '0');
 	_putchar((j % 10) + '0');
