471406f5be0089b7da23179785130e8ef44fab65
mo1699 <mo1699@gmail.com>
2023-09-06 21:52:37 +0200

    C - Hello, World

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,25 +8,27 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int i;
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				printf("%d\n", i);
-		}
-	} else if (n >= 98)
+	for (i = n ; i <= 98 ; i++)
 	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
+		if (i != 98)
+			printf("%d, ", i);
+		else if (i == 98)
+			printf("%d", i);
 	}
+	}
+	else if (n >= 98)
+	{
+	for (i = n ; i >= 98 ; i--)
+	{
+		if (i != 98)
+			printf("%d, ", i);
+		else if (i == 98)
+			printf("%d", i);
+	}
+}
+	printf("\n");
 }
