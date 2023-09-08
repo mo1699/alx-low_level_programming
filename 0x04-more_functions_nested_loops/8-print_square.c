c2b8da45a48e7b5ffecc31ffa6525233524acb26
mo1699 <mo1699@gmail.com>
2023-09-08 15:31:36 +0200

    C - Hello, World

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,4 @@
-#include "main.h"
+"main.h"
 
 /**
  * print_square - prints a square, followed by a new line;
@@ -12,10 +12,10 @@ void print_square(int size)
 		_putchar('\n');
 	else
 	{
-	for (i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
+			for (j = 0; j < size; j++)
+				_putchar('#');
 		_putchar('\n');
 	}
 	}
