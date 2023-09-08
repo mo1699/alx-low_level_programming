0467d993a8f44b52790acc7e568ebcf3e2baeed8
mo1699 <mo1699@gmail.com>
2023-09-08 04:10:27 +0200

    C - Hello, World

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,14 @@
 void print_diagonal(int n)
 {
 	int i, j;
-	
+
 	if (n <= 0)
 		_putchar('\n');
 	else if (n > 0)
 	{
-	for (i = 0;i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = 0;j < i; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
