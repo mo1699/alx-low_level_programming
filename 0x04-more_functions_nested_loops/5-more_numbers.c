53a3988540ef72f3aa0c8eeca00ef85dd002c765
mo1699 <mo1699@gmail.com>
2023-09-08 03:16:38 +0200

    C - Hello, World

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@ void more_numbers(void)
 {
 	int i, j;
 
- for (i = 0 ; i < 10; i++)
- {
-	 for (j = 0; j <= 14; j++)
+	for (i = 1; i <= 10; i++)
 	{
-	if (j > 9)
-		_putchar((j / 10) + '0');
-	_putchar((j % 10) + '0');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar("\n");
 	}
-	_putchar('\n');
-}
 }
