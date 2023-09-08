9810baded09742efd8f3d53121d48682fdc41e28
mo1699 <mo1699@gmail.com>
2023-09-08 22:52:21 +0200

    C - Hello, World

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,26 +6,17 @@
  */
 void print_triangle(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+	int i, j, k;
 
+	if (size <= 0)
+		_putchar('\n');
+	else
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < size; j++)
-			{
+			for (j = 0; j < size - i; j++)
 				_putchar(' ');
-			}
-
-			for (j = 1; j <= i; j++)
-			{
+			for (k = 0; k < i ; k++)
 				_putchar('#');
-			}
-
 			_putchar('\n');
 		}
-	}
 }
