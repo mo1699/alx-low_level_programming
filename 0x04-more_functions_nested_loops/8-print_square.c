b0708ade8c439801bd6ae8c3f436b19faf6e73d4
mo1699 <mo1699@gmail.com>
2023-09-08 14:22:03 +0200

    C - Hello, World

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,17 +8,19 @@ void print_square(int size)
 {
 	int i, j;
 
-	while (size > 0)
+	do
 	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
-			}
+		}
 			_putchar('\n');
 }
 }
+	while (size > 0);
+
 	if (size <= 0)
 		_putchar('\n');	
 }
