40dff8b3b5bba50f9ef7542a722735800e7c0de9
mo1699 <mo1699@gmail.com>
2023-09-15 00:29:10 +0200

    C - Hello, World

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,9 +14,8 @@ char *_strcpy(char *dest, char *src)
 	int i, j;
 
 	for (i = 0; src[i] != '\0'; i++)
-		;
-	for (j = 0; dest[j] != '\0'; j++)
-		dest[i] = src[j];
+		dest[i] = src[i];
+	dest[i] ='\0';
 
 	return (dest);
 }
