e8f0c7ee8332d5dadd6a7ae0c656f679576b9c7e
mo1699 <mo1699@gmail.com>
2023-09-15 17:21:00 +0200

    C - Hello, World

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-
-	while (src[i] != '\0' && i < n)
-	{
+	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
-		i++;
-	}
-
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	dest[i] = '\0';
 
 	return (dest);
 }
