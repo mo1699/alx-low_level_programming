16647cbbd7d91d80a5f7a7f67abe785f2efdf3a3
mo1699 <mo1699@gmail.com>
2023-09-15 15:13:27 +0200

    C - Hello, World

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,9 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	       ;
-	for (j = 0; dest[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 		dest[j+i] = src [j];
 
 	dest[j+i] = '\0';
