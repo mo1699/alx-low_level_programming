3a61939e13553038f7088f31986f1af2d224b83d
mo1699 <mo1699@gmail.com>
2023-09-15 14:34:14 +0200

    C - Hello, World

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,9 +17,9 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; src[i] != '\0'; i++)
 	       ;
 	for (j = 0; dest[j] != '\0'; j++)
-		dest[j] = src [i];
+		dest[i+j] = src [j];
 
-	dest[j] = '\0';
+	dest[i+j] = '\0';
 
 	return (dest);
 }
