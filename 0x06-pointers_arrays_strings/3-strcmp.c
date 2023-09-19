4a1acdec37dfd37dff93758bea0be7131cf02a0a
mo1699 <mo1699@gmail.com>
2023-09-19 10:33:33 +0200

    C - Hello, World

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,15 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	int len = 0;
+	int i, k = 0;
 
-	while (*s1)
-	{
-		if (*s1 != *s2)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		if (s1 != s2)
 		{
-			len = ((int)*s1 - 48) - ((int)*s2 - 48);
+			k = s1 - s2;
 			break;
 		}
-		s1++;
-		s2++;
-	}
+	if (s1 != s2)
+		k = s1 - s2;
+	return	(k);
 }
