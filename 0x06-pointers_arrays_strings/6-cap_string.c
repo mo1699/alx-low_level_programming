cb8206c69fc15badaa57b9515d0a8f1964fb8723
mo1699 <mo1699@gmail.com>
2023-09-19 11:55:13 +0200

    C - Hello, World

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
+			s[i] = s[i] - 32;
 
 		for (j = 0; j < 13; j++)
 		{
@@ -24,7 +24,7 @@ char *cap_string(char *s)
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
-					s[i + 1] -= 32;
+					s[i + 1] = s[i + 1] -  32;
 				}
 			}
 		}
