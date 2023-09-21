49a5a13310dd231bb6138c5ae38fb8de01421d37
Mohamed Mahmoud <133320326+mo1699@users.noreply.github.com>
2023-09-21 10:18:45 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,11 +17,8 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (!needle[j])
-			return (&haystack[i]);
+			if (haystack[i] == needle[j])
+				return (&haystack[i]);
 	}
 	return (NULL);
 }
