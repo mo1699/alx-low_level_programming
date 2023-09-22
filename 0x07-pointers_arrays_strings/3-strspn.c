8e99d534db1e32399da17ee42a125e9e5d3c6a6d
mo1699 <mo1699@gmail.com>
2023-09-22 23:11:24 +0200

    C - Hello, World

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,16 +10,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, f = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
-			if (s[i] != accept[j])
-			{
-				return (i);
+			if (s[i] == accept[j])
 
-			}
+				f++;
+			else
+				return (f);
+
 	}
-	return (0);
+	return (f);
 }
