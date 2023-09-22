8992fb306fc9efceafc82ade2bf87c0d7e5dd8d6
mo1699 <mo1699@gmail.com>
2023-09-23 00:04:10 +0200

    C - Hello, World

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,14 +13,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (j = 0; needle[j] != '\0'; j++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (i = 0; haystack[i] != '\0'; i++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
-				return (&haystack[i]);
-			break;
-	}
+			if (haystack[i] i= needle[j])
+					break;
+			else 
+				return &haystack[i]
+	}			
 
 }
 
