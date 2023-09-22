ae64d4b13ae14ac6f370b7abe4b4926e7bbed15a
mo1699 <mo1699@gmail.com>
2023-09-22 17:18:17 +0200

    C - Hello, World

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,12 @@
  */
 char *_strchr(char *s, char c)
 {
-		int i, k = i + 1;
+		int i, j;
 
-<<<<<<< HEAD
+
 		for (i = 0; s[i] != '\0'; i++)
 			;
 		for (j = 0; j <= i + 1; j++)
-=======
-		for (i = 0; s[k] != '\0'; i++)
->>>>>>> 25950ff5b1a0be640b25cffe211d177f1f90e85b
 		{
 			if (s[i] == c)
 				return (&s[i]);
