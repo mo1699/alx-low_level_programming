f5b72253096dff4d7465d7cd0c37844ccf67b0b0
mo1699 <mo1699@gmail.com>
2023-09-22 17:10:51 +0200

    C - Hello, World

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,20 +11,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j, k;
+	int i, j;
 
-    for (k = 0; k <= i + 1; k++)
-        ;
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[k] == accept[j])
-            {
-                return (&s[k]);
-            }
-        }
-    }
-
-    return (NULL);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (NULL);
 }
