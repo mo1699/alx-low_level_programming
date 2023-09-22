9d447c00dc785085119e53f49036a97764f40d82
mo1699 <mo1699@gmail.com>
2023-09-23 00:37:21 +0200

    C - Hello, World

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,7 +19,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum += a[(size * i) + i];
-		sum1 += a[i + (size - 1 -i)];
+		sum1 += a[(size - 1 -i) + i];
 	}
 
 	printf("%d, %d\n", sum, sum1);
