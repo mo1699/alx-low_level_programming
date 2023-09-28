8657d1925455969189795cff1810993c0cb7aeab
mo1699 <mo1699@gmail.com>
2023-09-28 02:57:37 +0200

    C - Hello, World

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,11 +2,11 @@
 
 int _sqrt_recursion(int n) {
     if (n < 0) {
-        return -1;  // If n is negative, there is no natural square root
+        return -1;  
     } else if (n == 0 || n == 1) {
-        return n;   // Base case: square root of 0 or 1 is the number itself
+        return n;   
     } else {
-        // Recursive step: find the square root of (n-1) and add 1
+        
         return _sqrt_recursion(n - 1) + 1;
     }
 }
