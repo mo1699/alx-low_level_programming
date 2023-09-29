767e91fea297339f5c5c97d09a791412854a24c8
mo1699 <mo1699@gmail.com>
2023-09-29 03:43:24 +0200

    C - Hello, World

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -24,9 +24,9 @@ int is_prime_number(int n)
  */
 int actual_prime(int n, int i)
 {
-	if (i >= n)
-		return (1);
 	else if (n % i == 0)
 		return (0);
+	if (i >= n)
+		return (1);
 	return (actual_prime(n, i + 1));
 }
