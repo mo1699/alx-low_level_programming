7fc30720f1853ce5c9b650f7c53115d84af417d7
mo1699 <mo1699@gmail.com>
2023-10-03 11:42:32 +0200

    C - Hello, World

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,23 +12,14 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	unsigned int i = 0;
+	int i;
 
 	if (size == 0)
-		return (NULL);
-
-	p = (char *) malloc(sizeof(char) * size);
-
+		return ("\0");
+	p = malloc(sizeof(c) * size);
 	if (p == NULL)
-		return (0);
-
-	while (i < size)
-	{
-		*(p + i) = c;
-		i++;
-	}
-
-	*(p + i) = '\0';
-
+		return ("\0");
+	for (i = 0; i < size; i++)
+		p[i] = c;
 	return (p);
 }
