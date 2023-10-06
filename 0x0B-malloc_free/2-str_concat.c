ca455e24dd27f92b3a08ec08b039012f9df6bbc0
mo1699 <mo1699@gmail.com>
2023-10-06 23:20:32 +0200

    C - Hello, World

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,36 +11,30 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i, j;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	s3 = malloc(sizeof(char) * (i + j + 1));
 	if (s3 == NULL)
 		return (NULL);
 
-	i = 0;
-	j = 0;
-
 	if (s1)
 	{
-		while (i < len1)
+		for (i = 0; s1[i] != '\0'; i++)
 		{
 			s3[i] = s1[i];
-			i++;
 		}
 	}
 
 	if (s2)
 	{
-		while (i < (len1 + len2))
+		for (j = 0; s2[j] != '\0'; j++)
 		{
-			s3[i] = s2[j];
-			i++;
-			j++;
+			s3[i + j] = s2[j];
 		}
 	}
 	s3[i] = '\0';
