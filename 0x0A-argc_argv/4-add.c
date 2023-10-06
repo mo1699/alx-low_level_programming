5dd8ba9dd13b061dda37cf69a871bf5127384fa9
mo1699 <mo1699@gmail.com>
2023-10-06 16:38:28 +0200

    C - Hello, World

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,12 +14,14 @@ int main(int argc, char *argv[])
 	int i, j, k, sum = 0, num;
 
 	for (i = 1; i < argc; i++)
+	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
 				printf("Error\n");
 				return	(1);
 			}
+	}
 		for (k = 0; k < argc; k++)
 		{
 			num = atoi(argv[argc - 1]);
