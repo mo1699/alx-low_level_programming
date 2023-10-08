28ff5a2a2ca41b45a5a67bb50898976c4d6a38cc
mo1699 <mo1699@gmail.com>
2023-10-08 22:57:01 +0200

    C - Hello, World

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,8 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
-			free(arr);
+			for (i = 0 ;i < height; i++)
+				free(arr);
 		return (NULL);
 		}
 	}
