e45ef851cb612bbe1963ff5247a6086cc9f16fbe
mo1699 <mo1699@gmail.com>
2023-10-09 18:33:59 +0200

    C - Hello, World

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,16 +14,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int i = 0, j = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
+	if (s1)
+		for (i = 0; s1[i] != '\0'; i++)
+			;
 	s = malloc(sizeof(char) * (i + n + 1));
 	if (s == NULL)
 		return (NULL);
-
-	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
-	for (j = 0; j < n; j++)
-		s[i + j] = s2[j];
+	if (s1)
+		for (i = 0; s1[i] != '\0'; i++)
+			s[i] = s1[i];
+	if (s2)
+		for (j = 0; j < n; j++)
+			s[i + j] = s2[j];
 	s[i + j + 1] = '\0';
 	return (s);
 }
