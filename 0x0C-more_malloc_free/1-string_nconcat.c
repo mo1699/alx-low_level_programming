eed67609f41001bff99294fcd57d8dbcb5b95bac
mo1699 <mo1699@gmail.com>
2023-10-09 18:45:50 +0200

    C - Hello, World

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,6 +32,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 != NULL)
 		for (j = 0; j < n; j++)
 			s[i + j] = s2[j];
-	s[i + j + 1] = '\0';
+	s[i + j] = '\0';
 	return (s);
 }
