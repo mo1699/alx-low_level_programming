ceb1bf4d742f7a029e69c080d0be8aa4199f86c6
mo1699 <mo1699@gmail.com>
2023-10-12 02:28:56 +0200

    C - Hello, World

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,16 +12,3 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
-
-	if (array != NULL && cmp != NULL)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
-	}
-
-	return (-1);
-}
