03bb83289d84263ebce62d2773f8331159952300
mo1699 <mo1699@gmail.com>
2023-10-21 05:09:05 +0200

    C - Hello, World

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
+	list_t *temp = *head;
 	unsigned int len = 0;
 
 	while (str[len])
@@ -25,15 +26,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->len = len;
 	new->next = NULL;
 
-	if (!(*head))
+	if (!temp)
 	{
 		*head = new;
 		return (new);
 	}
-	while ((*head)->next)
-		*head = (*head)->next;
-
-	(*head)->next = new;
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+		temp->next = new;
 
 	return (new);
 }
