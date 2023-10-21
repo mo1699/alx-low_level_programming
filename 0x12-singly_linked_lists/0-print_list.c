4879254dbcc4dca52353a17c80536b883038a9de
mo1699 <mo1699@gmail.com>
2023-10-21 03:18:49 +0200

    C - Hello, World

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,11 +15,12 @@ size_t print_list(const list_t *h)
 
 	for (i = 0; h != NULL; i++)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)");
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 	}
+
 	return (i);
 }
