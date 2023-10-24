6f09cd78b5ce7dbbe0adb50a0529cf26ddc99a58
mo1699 <mo1699@gmail.com>
2023-10-25 01:14:06 +0200

    C - Hello, World

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,5 +16,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	/*intialize the new node */
 	new_node->n = n;
 	new_node->next = *head;
-	return (new_node);
+	new_node = *head;
+	return (*head);
 }
