878b5561aec3ebfd218c6f1b64f517b2bec6bc56
mo1699 <mo1699@gmail.com>
2023-10-25 02:55:51 +0200

    C - Hello, World

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,17 +7,15 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
+	/*listint_t *temp;*/
 	int i;
 /*check for NULL entry */
 	if (!*head)
 		return (0);
 	/*assign the content &the next node address before deleting */
 	i = (*head)->n;
-	temp = (*head)->next;
 /*free the node */
 	free(*head);
 /*reassign the header*/
-	*head = temp;
 	return (i);
 }
