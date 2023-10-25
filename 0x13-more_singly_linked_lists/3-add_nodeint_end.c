076829cb06d7963cfcef9fb23accc9415f628323
mo1699 <mo1699@gmail.com>
2023-10-25 02:02:38 +0200

    C - Hello, World

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new_node, *temp;
 /* create the new node*/
 	new_node = malloc(sizeof(listint_t));
 
@@ -24,8 +24,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while ((*head)->next)
-		*head = (*head)->next;
-	(*head)->next = new_node;
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
