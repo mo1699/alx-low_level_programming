7063bc478772ada8fa682fcada456657f1d44eaf
mo1699 <mo1699@gmail.com>
2023-11-06 04:43:50 +0200

    C - Hello, World

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		;
 	if (!filename)
 		return (-1);
-	o = open(filename, O_WRONLY, O_APPEND);
+	o = open(filename, O_RDWR | O_APPEND);
 	if (text_content)
 		w = write(o, text_content, strlen(text_content));
 	if (w == -1 || o == -1)
